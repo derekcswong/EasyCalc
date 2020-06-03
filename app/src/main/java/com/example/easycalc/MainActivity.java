7ef6a57c4949831d91201bb4086f33a54612bebd
derekcswong <derek4b4@gmail.com>
2020-06-02 20:55:26 -0700

    update

--- app/src/main/java/com/example/easycalc/MainActivity.java
+++ app/src/main/java/com/example/easycalc/MainActivity.java
@@ -5,70 +5,159 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String[] operations = {"add", "subtract", "multiply", "divide"};
-    String selectedOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner operationSpinner = (Spinner) findViewById(R.id.operationSpinner);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, operations);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        operationSpinner.setAdapter(arrayAdapter);
+        final TextView result_textView = (TextView) findViewById(R.id.number1);
+        Button equals = (Button) findViewById(R.id.equal);
+        Button clear = (Button) findViewById(R.id.clear);
+        Button decimal = (Button) findViewById(R.id.decimal);
+        Button zero = (Button) findViewById(R.id.zero);
+        Button one = (Button) findViewById(R.id.one);
+        Button two = (Button) findViewById(R.id.two);
+        Button three = (Button) findViewById(R.id.three);
+        Button four = (Button) findViewById(R.id.four);
+        Button five = (Button) findViewById(R.id.five);
+        Button six = (Button) findViewById(R.id.six);
+        Button seven = (Button) findViewById(R.id.seven);
+        Button eight = (Button) findViewById(R.id.eight);
+        Button nine = (Button) findViewById(R.id.nine);
+        Button divide = (Button) findViewById(R.id.divide);
+        Button multiply = (Button) findViewById(R.id.multiply);
+        Button subtract = (Button) findViewById(R.id.subtract);
+        Button mod = (Button) findViewById(R.id.mod);
+        Button add = (Button) findViewById(R.id.add);
+        Button left_bracket = (Button) findViewById(R.id.bracket_left);
+        Button right_bracket = (Button) findViewById(R.id.bracket_right);
 
-        operationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        equals.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedOperation = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // do nothing
+            public void onClick(View v) {
+                result_textView.setSelected(true);
             }
         });
 
-
-        Button computeBtn = findViewById(R.id.computeBTN);
-        computeBtn.setOnClickListener(new View.OnClickListener() {
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float res;
-                EditText firstNum = (EditText) findViewById(R.id.number1);
-                EditText secondNum = (EditText) findViewById(R.id.number2);
-                TextView resultTextView = (TextView) findViewById(R.id.result_textView);
-                try {
-                    float f = Float.parseFloat(firstNum.getText().toString());
-                    float s = Float.parseFloat(secondNum.getText().toString());
-                    switch(selectedOperation) {
-                        case "add":
-                            res = f + s;
-                            break;
-                        case "subtract":
-                            res = f - s;
-                            break;
-                        case "multiply":
-                            res = f*s;
-                            break;
-                        case "divide":
-                            res = f/s;
-                            break;
-                        default: res = -1;
-                    }
-                    resultTextView.setText(Float.toString(res));
-                } catch (Exception e) {
-                    resultTextView.setText("Please enter valid numbers");
-                }
+                result_textView.setText("");
+            }
+        });
+
+        decimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(".");
+            }
+        });
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("0");
+            }
+        });
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("1");
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("9");
+            }
+        });
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(" / ");
+            }
+        });
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(" x ");
+            }
+        });
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(" - ");
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(" + ");
+            }
+        });
+        mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(" % ");
+            }
+        });
+        right_bracket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append(")");
+            }
+        });
+        left_bracket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result_textView.append("(");
             }
         });
 
